7556f0b0612603f0117c1c4b93498373b1dfbe5a
ravinthiranpartheepan1407 <ravinthiranpartheepan1407@gmail.com>
2024-01-06 16:45:57 +0200

    V1.0.5: Standard Deviation

---
+++ stdevOp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vertezml
+{
+    public static class stdevOp
+    {
+        public static double stdev(List<double> x)
+        {
+            int elements;
+            double mean;
+            double sqrdSum = 0;
+            double sum = 0;
+            for(elements = 0; elements < x.Count; elements++)
+            {
+                sum += x[elements];
+            }
+            mean = sum / x.Count;
+            for(elements = 0; elements < x.Count; elements++)
+            {
+                double diff = x[elements] - mean;
+                double powDiff = Math.Pow(diff, 2);
+                sqrdSum += powDiff;
+            }
+            double calc = sqrdSum / x.Count;
+            double stdOut = Math.Sqrt(calc);
+            return stdOut;
+        }
+    }
+}
